e5edc879817154a316f165fe55aa3b6fe6c71904
Yash Patange <yash.frost1997@gmail.com>
2020-10-14 00:02:31 -0400

    COVID-19 Tracker, first commit!

---
+++ src/test/java/com/testproject/trackerproject/TrackerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testproject.trackerproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
